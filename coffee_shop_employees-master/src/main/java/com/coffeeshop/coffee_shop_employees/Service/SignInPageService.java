8c7beba7c048ce1bc0ce4afa115ba62a9f041d49
1103915 <1103915@stud.uot.edu.pk>
2023-05-13 00:48:52 +0500

    Updated SignInPageService

--- coffee_shop_employees-master/src/main/java/com/coffeeshop/coffee_shop_employees/Service/SignInPageService.java
+++ coffee_shop_employees-master/src/main/java/com/coffeeshop/coffee_shop_employees/Service/SignInPageService.java
@@ -4,14 +4,17 @@ package com.coffeeshop.coffee_shop_employees.Service;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import com.coffeeshop.coffee_shop_employees.Model.SignInPage;
 import com.coffeeshop.coffee_shop_employees.Model.UserDetails;
-@Repository
-public interface SignInPageService extends JpaRepository<SignInPage,Long>{
+@Service
+public class SignInPageService{
    
-   
+    List<UserDetails> storage = new ArrayList<>();
+    public List<UserDetails> GetDetails(){
+        return this.storage;
+    }
+    public void AddDetails(UserDetails model){
+        storage.add(model);
+    }
 }
