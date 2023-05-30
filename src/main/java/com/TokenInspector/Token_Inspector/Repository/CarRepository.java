a12a30487cf05a4f8476abc53bcfbb80e3bd2c14
FaisaljanBaloch <faisaljanbaloch@gmail.com>
2023-05-30 22:55:39 +0500

    feat: make search bar functional with search by plate number

--- src/main/java/com/TokenInspector/Token_Inspector/Repository/CarRepository.java
+++ src/main/java/com/TokenInspector/Token_Inspector/Repository/CarRepository.java
@@ -1,12 +1,13 @@
 package com.TokenInspector.Token_Inspector.Repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 
 import com.TokenInspector.Token_Inspector.Model.CarRegistrationDetails;
 
 @Repository
-public interface CarRepository extends JpaRepository<CarRegistrationDetails,Long>{
-
+public interface CarRepository extends JpaRepository<CarRegistrationDetails, Long> {
+    List<CarRegistrationDetails> findCarRegistrationDetailsByPlateNumber(String plateNumber);
 }
